73e25bf6a0d0e89773cabbff336df591a29fbde8
23021632NguyenQuangMinh <23021632@vnu.edu.vn>
2025-05-16 00:58:42 +0700

    fix compute_forwarding_table

--- LSrouter.py
+++ LSrouter.py
@@ -1,7 +1,7 @@
 ####################################################
 # LSrouter.py
 # Name: Link State Router Implementation
-# HUID: 23021628 + 23021632
+# HUID:
 #####################################################
 
 import heapq
@@ -31,16 +31,20 @@ class LSrouter(Router):
     def handle_packet(self, port, packet):
         """Process incoming packet."""
         if packet.is_traceroute:
+            # data packet
             if packet.dst_addr in self.forwarding_table:
                 out_port = self.forwarding_table[packet.dst_addr]
                 self.send(out_port, packet)
         else:
+            #routing packet
             try:
                 router_addr, seq_number, link_state = json.loads(packet.content)
                 if router_addr not in self.seq_numbers or seq_number > self.seq_numbers[router_addr]:
                     self.seq_numbers[router_addr] = seq_number
                     self.topology[router_addr] = link_state
+
                     self.compute_forwarding_table()
+
                     for neighbor_port in self.links:
                         if neighbor_port != port:
                             self.send(neighbor_port, packet)
@@ -59,8 +63,10 @@ class LSrouter(Router):
         """Handle removed link."""
         if port in self.port_to_neighbor:
             endpoint = self.port_to_neighbor[port]
+
             del self.port_to_neighbor[port]
             del self.neighbor_to_port[endpoint]
+
             if endpoint in self.topology[self.addr]:
                 del self.topology[self.addr][endpoint]
             self.compute_forwarding_table()
@@ -79,6 +85,7 @@ class LSrouter(Router):
         self.seq_numbers[self.addr] += 1
         link_state_info = [self.addr, self.seq_numbers[self.addr], self.topology[self.addr]]
         content = json.dumps(link_state_info)
+
         for port in self.links:
             packet = Packet(Packet.ROUTING, self.addr, self.port_to_neighbor.get(port, "Unknown"), content)
             self.send(port, packet)
@@ -86,6 +93,7 @@ class LSrouter(Router):
     def compute_forwarding_table(self):
         """Compute shortest paths using Dijkstra's algorithm."""
         dist = {self.addr: 0}
+        prev = {}
         first_hop = {}
         pq = [(0, self.addr)]
 
@@ -93,16 +101,19 @@ class LSrouter(Router):
             current_dist, current = heapq.heappop(pq)
             if current in dist and current_dist > dist[current]:
                 continue
-            for neighbor, cost in self.topology[current].items():
-                new_dist = current_dist + cost
-                if neighbor not in dist or new_dist < dist[neighbor]:
-                    heapq.heappush(pq, (new_dist, neighbor))
-                    dist[neighbor] = new_dist
-                    if current == self.addr:
-                        first_hop[neighbor] = neighbor
-                    elif current in first_hop:
-                        first_hop[neighbor] = current
+            if current in self.topology:
+                for neighbor, cost in self.topology[current].items():
+                    new_dist = current_dist + cost
+                    if neighbor not in dist or new_dist < dist[neighbor]:
+                        dist[neighbor] = new_dist
+                        prev[neighbor] = current
+                        if current == self.addr:
+                            first_hop[neighbor] = neighbor
+                        elif current in first_hop:
+                            first_hop[neighbor] = first_hop[current]
+                        heapq.heappush(pq, (new_dist, neighbor))
 
+        self.forwarding_table = {}
         for dst in first_hop:
             if first_hop[dst] in self.neighbor_to_port:
                 self.forwarding_table[dst] = self.neighbor_to_port[first_hop[dst]]
